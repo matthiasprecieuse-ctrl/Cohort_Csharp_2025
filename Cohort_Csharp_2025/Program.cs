560e41eed779ae834ce6453a3f750afd48df304b
Precious Matthias <Preciousmatthias1@outlook.com>
2025-09-19 09:36:08 +0100

    Add project files.

---
+++ Cohort_Csharp_2025/Program.cs
@@ -0,0 +1,48 @@
+﻿//See https://aka.ms/new-console-template for more information
+
+//represent a collection of objects that can be individually accessed by index.
+// Linqs always work with IEnumerable and return IEnumerable
+// Linqs always return collections of data but not list collection. The collection is always IEnumerable in IEnumerable format
+
+//using Cohort_Csharp_2025;
+
+//IList<string> wordlist = new List<string>()
+//{
+//    "C# course",
+//    "C++ Tutorial",
+//    "Java course",
+//    "Python course",
+//    "Swift course",
+//    "Flutter Tutorial",
+//    "Kotlin Tutorial"
+//};
+
+//query of Linqs
+//it states that from the list above select the words that contains c# and print them
+//var result = from s in wordlist
+//             where s.Contains("Tutorial")
+//             select s;
+//foreach (var word in result)
+//{
+//    Console.WriteLine(word);
+//}
+
+//Method in Linqs
+//filtering : filtering means selecting a subset of elements from a collection based on a specified condition or criteria.
+//projection
+//ordering
+//grouping
+//set operators
+//joning 
+//element operators
+//aggregate operators
+//quantifiers
+//conversion methods
+
+
+//Filtering filtering = new Filtering();
+//filtering.WhereValue();
+
+using Cohort_Csharp_2025;
+
+projection.SelectValue();
