931dd89591c2c6f16b9730130b1fcc1dfaf375f3
Precious Matthias <Preciousmatthias1@outlook.com>
2025-09-19 16:51:02 +0100

    Dictionary

---
+++ Cohort_Csharp_2025/Footballers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Footballers
+{
+    public int Number { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; }
+
+    public static List<Footballers> GetAll()
+    {
+        return new List<Footballers>
+            {
+                new Footballers { Number = 24, FirstName = "André",   LastName = "Onana",   Role = "Goalkeeper" },
+                new Footballers { Number = 20, FirstName = "Diogo",   LastName = "Dalot",   Role = "Defender" },
+                new Footballers { Number = 19, FirstName = "Raphaël", LastName = "Varane",  Role = "Defender" },
+                new Footballers { Number = 6,  FirstName = "Lisandro",LastName = "Martínez",Role = "Defender" },
+                new Footballers { Number = 18, FirstName = "Casemiro",LastName = "Silva",   Role = "Midfielder" },
+                new Footballers { Number = 8,  FirstName = "Bruno",   LastName = "Fernandes",Role = "Midfielder" },
+                new Footballers { Number = 21, FirstName = "Antony",  LastName = "Matheus", Role = "Forward" },
+                new Footballers { Number = 10, FirstName = "Marcus",  LastName = "Rashford",Role = "Forward" },
+                new Footballers { Number = 11, FirstName = "Rasmus",  LastName = "Højlund", Role = "Forward" },
+                new Footballers { Number = 7,  FirstName = "Mason",   LastName = "Mount",   Role = "Midfielder" }
+            };
+    }
+}
---
+++ Cohort_Csharp_2025/SetOperatorsFootballers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cohort_Csharp_2025
+{
+    
+    public static class SetOperatorsFootballers
+    {
+       
+        public static void SelectValue()
+        {
+            
+            var data = (from f in Footballers.GetAll()
+                        select new { f.FirstName, f.LastName })
+                       .ToList();
+
+            Console.WriteLine("Manchester United Players (First and Last Names):");
+            foreach (var player in data)
+            {
+                Console.WriteLine($"{player.FirstName} {player.LastName}");
+            }
+        }
+    }
+}
